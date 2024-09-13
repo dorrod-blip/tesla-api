2c19a211330c8009d155de61e73e5b12dfbd45f4
dorrod-blip <supercoder0829@gmail.com>
2024-09-13 14:24:52 +0200

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: 'https://tesla-vehicle-app.vercel.app', // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow credentials if needed
   });
